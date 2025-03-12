afa51bde55e567afa8a9089157c12e64c5de36d2
Arnav Khandelwal <arnav181104@gmail.com>
2025-03-12 11:40:13 +0530

    changing browserRouter to HashRouter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Navigation from './components/Navigation';
 import Home from './components/Home';
