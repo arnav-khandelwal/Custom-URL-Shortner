e23e3eaf3937eb12f5cf6e162bc2d1ea4ebe7337
Arnav Khandelwal <arnav181104@gmail.com>
2025-03-12 10:06:12 +0530

    added mongoDB atlas connection

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,15 @@
 import express from "express";
 import urlRoute from './routes/url.js';
 import connectToMongoDB from "./connection.js";
-import URL from "./models/url.js";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const app = express();
 const PORT = 8001;
 
-connectToMongoDB('mongodb://127.0.0.1:27017/short-url').then(console.log(`Connected to MongoDB!`));
+connectToMongoDB(process.env.MONGO_URI).then(() => console.log(`Connected to MongoDB Atlas!`));
 
 app.use(cors({
     origin: "http://localhost:5173",  
@@ -18,4 +21,4 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/url', urlRoute);
 
-app.listen(PORT, () => console.log(`server started at: ${PORT}`));
+app.listen(PORT, () => console.log(`Server started at: ${PORT}`));
